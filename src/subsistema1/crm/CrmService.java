98d09d01264a582bdfe057a9596470ab333457f9
Jaqueline Costa <143290999+jaquecoxta@users.noreply.github.com>
2023-10-19 21:50:11 -0300

    Update CrmService.java
    Modificação do código para melhoria.

--- src/subsistema1/crm/CrmService.java
+++ src/subsistema1/crm/CrmService.java
@@ -2,15 +2,35 @@ package subsistema1.crm;
 
 public class CrmService {
 
-	private CrmService() {
-		super();
-	}
-	
-	public static void gravarCliente(String nome, String cep, String cidade, String estado) {
-		System.out.println("Cliente salvo no sistema de CRM:");
-		System.out.println(nome);
-		System.out.println(cep);
-		System.out.println(cidade);
-		System.out.println(estado);
-	}
+    private CrmService() {
+        // Construtor privado para evitar instanciação da classe
+    }
+
+    /**
+     * Grava informações do cliente no sistema de CRM.
+     *
+     * @param nome   Nome do cliente.
+     * @param cep    CEP do cliente.
+     * @param cidade Cidade do cliente.
+     * @param estado Estado do cliente.
+     * @throws IllegalArgumentException Se algum dos parâmetros for nulo ou vazio.
+     */
+    public static void gravarCliente(String nome, String cep, String cidade, String estado) {
+        // Validação dos parâmetros de entrada
+        if (nome == null || nome.isEmpty() || cep == null || cep.isEmpty() ||
+                cidade == null || cidade.isEmpty() || estado == null || estado.isEmpty()) {
+            throw new IllegalArgumentException("Todos os parâmetros são obrigatórios.");
+        }
+
+        // Construção da mensagem usando StringBuilder
+        StringBuilder mensagem = new StringBuilder();
+        mensagem.append("Cliente salvo no sistema de CRM:\n");
+        mensagem.append("Nome: ").append(nome).append("\n");
+        mensagem.append("CEP: ").append(cep).append("\n");
+        mensagem.append("Cidade: ").append(cidade).append("\n");
+        mensagem.append("Estado: ").append(estado).append("\n");
+
+        // Saída da mensagem
+        System.out.println(mensagem.toString());
+    }
 }
