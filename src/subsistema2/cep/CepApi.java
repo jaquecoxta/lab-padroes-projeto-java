19dac648b1ff9af7e44d3f3f9482cb4ca365661a
Jaqueline Costa <143290999+jaquecoxta@users.noreply.github.com>
2023-10-19 21:45:22 -0300

    Update CepApi.java
    Modificação no código para melhoria.

--- src/subsistema2/cep/CepApi.java
+++ src/subsistema2/cep/CepApi.java
@@ -2,21 +2,37 @@ package subsistema2.cep;
 
 public class CepApi {
 
-	private static CepApi instancia = new CepApi();
+    private static final String CIDADE_PADRAO = "Araraquara";
+    private static final String ESTADO_PADRAO = "SP";
+    private static CepApi instancia = new CepApi();
 
-	private CepApi() {
-		super();
-	}
+    private CepApi() {
+        super();
+    }
 
-	public static CepApi getInstancia() {
-		return instancia;
-	}
-	
-	public String recuperarCidade(String cep) {
-		return "Araraquara";
-	}
-	
-	public String recuperarEstado(String cep) {
-		return "SP";
-	}
+    public static CepApi getInstancia() {
+        return instancia;
+    }
+    public String recuperarCidade(String cep) {
+        // Lógica para recuperar a cidade associada ao CEP
+        // Capturar exceções, se houver, e tratar adequadamente
+        try {
+            // ...
+        } catch (Exception e) {
+            // Logar o erro ou retornar uma mensagem de erro amigável
+            return "Erro ao recuperar a cidade para o CEP fornecido.";
+        }
+        return CIDADE_PADRAO;
+    }
+    public String recuperarEstado(String cep) {
+        // Lógica para recuperar o estado associado ao CEP
+        // Capturar exceções, se houver, e tratar adequadamente
+        try {
+            // ...
+        } catch (Exception e) {
+            // Logar o erro ou retornar uma mensagem de erro amigável
+            return "Erro ao recuperar o estado para o CEP fornecido.";
+        }
+        return ESTADO_PADRAO;
+    }
 }
