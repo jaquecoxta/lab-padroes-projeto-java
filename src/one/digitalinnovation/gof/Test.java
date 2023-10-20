dfe1d66a659aac339c4ffb993916c0f9effbdd20
Jaqueline Costa <143290999+jaquecoxta@users.noreply.github.com>
2023-10-19 22:00:52 -0300

    Update Test.java
    Modificação no código para melhoria geral.

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -4,7 +4,6 @@ import one.digitalinnovation.gof.facade.Facade;
 import one.digitalinnovation.gof.singleton.SingletonEager;
 import one.digitalinnovation.gof.singleton.SingletonLazy;
 import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
-import one.digitalinnovation.gof.strategy.Comportamento;
 import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
 import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
 import one.digitalinnovation.gof.strategy.ComportamentoNormal;
@@ -12,46 +11,43 @@ import one.digitalinnovation.gof.strategy.Robo;
 
 public class Test {
 
-	public static void main(String[] args) {
-		
-		// Singleton
-		
-		SingletonLazy lazy = SingletonLazy.getInstancia();
-		System.out.println(lazy);
-		lazy = SingletonLazy.getInstancia();
-		System.out.println(lazy);
-		
-		SingletonEager eager = SingletonEager.getInstancia();
-		System.out.println(eager);
-		eager = SingletonEager.getInstancia();
-		System.out.println(eager);
-		
-		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
-		System.out.println(lazyHolder);
-		lazyHolder = SingletonLazyHolder.getInstancia();
-		System.out.println(lazyHolder);
-		
-		// Strategy
-		
-		Comportamento defensivo = new ComportamentoDefensivo();
-		Comportamento normal = new ComportamentoNormal();
-		Comportamento agressivo = new ComportamentoAgressivo();
-		
-		Robo robo = new Robo();
-		robo.setComportamento(normal);
-		robo.mover();
-		robo.mover();
-		robo.setComportamento(defensivo);
-		robo.mover();
-		robo.setComportamento(agressivo);
-		robo.mover();
-		robo.mover();
-		robo.mover();
-		
-		// Facade
-		
-		Facade facade = new Facade();
-		facade.migrarCliente("Venilton", "14801788");
-	}
+    public static void main(String[] args) {
+        // Teste Singleton
+        testSingleton();
 
+        // Teste Strategy
+        testStrategy();
+
+        // Teste Facade
+        testFacade();
+    }
+
+    private static void testSingleton() {
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println("Instância Singleton Lazy: " + lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println("Instância Singleton Eager: " + eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println("Instância Singleton Lazy Holder: " + lazyHolder);
+    }
+
+    private static void testStrategy() {
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+
+        for (int i = 0; i < 6; i++) {
+            robo.mover();
+        }
+    }
+
+    private static void testFacade() {
+        Facade facade = new Facade();
+        facade.migrarCliente("Jaqueline", "14801788");
+    }
 }
